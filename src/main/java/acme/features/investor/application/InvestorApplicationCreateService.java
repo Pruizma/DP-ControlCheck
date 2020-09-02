0cefed64a55e61d5bfb12ce60cb791f9a0cc7cd9
Pepe Ruiz-Mateos <43989024+Pruizma@users.noreply.github.com>
2020-09-02 13:06:23 +0200

    Update InvestorApplicationCreateService.java

--- src/main/java/acme/features/investor/application/InvestorApplicationCreateService.java
+++ src/main/java/acme/features/investor/application/InvestorApplicationCreateService.java
@@ -93,6 +93,17 @@ public class InvestorApplicationCreateService implements AbstractCreateService<I
 			boolean isEquals = newTicker.equals(invTicker);
 			errors.state(request, isEquals, "ticker", "i.a.error.activityTicker");
 		}
+		// Formato en €
+		if (!errors.hasErrors("moneyOffer")) {
+			boolean moneyCurrencyMax = entity.getMoneyOffer().getCurrency().equals("EUR") || entity.getMoneyOffer().getCurrency().equals("EUROS") || entity.getMoneyOffer().getCurrency().equals("€");
+			errors.state(request, moneyCurrencyMax, "moneyOffer", "a.o.error.money");
+		}
+
+		// Mínimo debe ser mayor que cero
+		if (!errors.hasErrors("moneyOffer")) {
+			boolean minBiggerThanZero = entity.getMoneyOffer().getAmount() >= 0;
+			errors.state(request, minBiggerThanZero, "moneyOffer", "a.o.error.plus");
+		}
 	}
 
 	@Override
